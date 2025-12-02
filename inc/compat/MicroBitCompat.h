8492b83d1e04242f3ac09b4da7ac1f61c9f0dc7f
Thomas Ball <thomas.j.ball@outlook.com>
2025-12-01 18:25:23 -0800

    more changes

--- inc/compat/MicroBitCompat.h
+++ inc/compat/MicroBitCompat.h
@@ -267,6 +267,14 @@ typedef codal::BitmapFont MicroBitFont;
 
 //#define MICROBIT_PANIC_ERROR_CHARS
 
+//
+// MBed legacy IDs - DO NOT USE
+// Note: These are actual integers to allow us to use __attribute__
+//
+const uint16_t MICROBIT_ID_MBED_INTERRUPT_IN __attribute__ ((deprecated)) = 80;
+const uint16_t MICROBIT_ID_MBED_PWM          __attribute__ ((deprecated)) = 81;
+const uint16_t MICROBIT_ID_MBED_TIMEOUT      __attribute__ ((deprecated)) = 82;
+const uint16_t MICROBIT_ID_MBED_TICKER       __attribute__ ((deprecated)) = 83;
 
 //
 // MICROBIT specific component IDs and event codes
@@ -344,9 +352,6 @@ typedef codal::BitmapFont MicroBitFont;
 #define MICROBIT_ID_IO_P20                                      ID_PIN_P20
 #define MICROBIT_ID_LOGO                                        ID_PIN_LOGO
 
-// For MakeCode extension compat. Delete ASAP.
-#define MICROBIT_ID_FACE                                        ID_PIN_FACE
-
 #define MICROBIT_ID_MESSAGE_BUS_LISTENER                        DEVICE_ID_MESSAGE_BUS_LISTENER
 #define MICROBIT_ID_MULTIBUTTON_ATTACH                          DEVICE_ID_MULTIBUTTON_ATTACH
 #define MICROBIT_ID_NOTIFY                                      DEVICE_ID_NOTIFY
@@ -356,16 +361,15 @@ typedef codal::BitmapFont MicroBitFont;
 #define MICROBIT_ID_SERIAL                                      DEVICE_ID_SERIAL
 #define MICROBIT_ID_THERMOMETER                                 DEVICE_ID_THERMOMETER
 
-#define MICROBIT_ID_PARTIAL_FLASHING                            36
-#define MICROBIT_ID_POWER_MANAGER                               37
-#define MICROBIT_ID_USB_FLASH_MANAGER                           38
+#define MICROBIT_ID_POWER_MANAGER                               DEVICE_ID_POWER_MANAGER
+#define MICROBIT_ID_PARTIAL_FLASHING                            DEVICE_ID_PARTIAL_FLASHING
+#define MICROBIT_ID_USB_FLASH_MANAGER                           DEVICE_ID_USB_FLASH_MANAGER
 
-#define MICROBIT_ID_VIRTUAL_SPEAKER_PIN                         39
+#define MICROBIT_ID_VIRTUAL_SPEAKER_PIN                         DEVICE_ID_VIRTUAL_SPEAKER_PIN
 
-#define MICROBIT_ID_MBED_INTERRUPT_IN                           40 
-#define MICROBIT_ID_MBED_PWM                                    41
-#define MICROBIT_ID_MBED_TIMEOUT                                42
-#define MICROBIT_ID_MBED_TICKER                                 43
+#define MICROBIT_ID_LOG                                         DEVICE_ID_LOG
+
+#define MICROBIT_ID_UTILITY                                     45
 
 #define MICROBIT_MAXIMUM_HEAPS                                  DEVICE_MAXIMUM_HEAPS
 #define MICROBIT_NESTED_HEAP_SIZE                               0
@@ -397,6 +401,13 @@ namespace codal
     uint16_t htons(uint16_t v);
 };
 
-using namespace codal;
+//
+// MicroBitButton has dependencies on the definitions here, so needs to be included
+// after these are defined to avoid having to redeclare everything needlessly or have
+// user code manually include this after MicroBitCompat.h
+//
+// Without this, we end up with circular dependencies.
+//
+#include "MicroBitButton.h"
 
 #endif
